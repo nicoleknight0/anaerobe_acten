2dc63c650a02fc8fec1f0f68c32000a81d82ea60
nicoleknight0 <nicoleknight0@gmail.com>
2015-06-16 14:53:03 -0700

    added code to make a histogram of activation energy values

--- mem1.R
+++ mem1.R
@@ -1,1 +1,18 @@
 #mixed effects model to determine activation energies for anaerobic organisms
+
+library(nlme)
+
+mdata<-read.csv("analysis_data.csv")
+mdata2<-mdata[,c("Dataset","X1_kT","ln_rate")]
+
+acten<-groupedData(ln_rate~X1_kT|Dataset, mdata2, order.groups=FALSE)
+test<-lmList(ln_rate~X1_kT|Dataset, data=mdata2, na.action=na.omit)
+pairs(test, id=0.05)
+
+hist(coef(test)[,2], breaks=100, xlab='slope', main = 'Slopes from lmList individual model fitting')
+
+#import coefficient data from above groupedData as csv
+
+cdata<-read.csv("coef_data.csv")
+
+hist(coef(test)[,2],breaks=30,xlab="Ea")
