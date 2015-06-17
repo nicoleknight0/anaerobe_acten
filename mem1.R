21db1fbb4397a1c50645732dc967fac9d96fb9b6
nicoleknight0 <nicoleknight0@gmail.com>
2015-06-17 11:21:27 -0700

    added a couple of mems and tests.

--- mem1.R
+++ mem1.R
@@ -1,9 +1,14 @@
 #mixed effects model to determine activation energies for anaerobic organisms
 
-library(nlme)
+#library(nlme)
+library(ggplot2)
 
 mdata<-read.csv("analysis_data.csv")
-mdata2<-mdata[,c("Dataset","X1_kT","ln_rate")]
+pur_data<-subset(mdata,Culture="Pure")
+mdata2<-pur_data[,c("Dataset","X1_kT","ln_rate")]
+
+
+
 
 acten<-groupedData(ln_rate~X1_kT|Dataset, mdata2, order.groups=FALSE)
 test<-lmList(ln_rate~X1_kT|Dataset, data=mdata2, na.action=na.omit)
@@ -11,8 +16,24 @@ pairs(test, id=0.05)
 
 hist(coef(test)[,2], breaks=100, xlab='slope', main = 'Slopes from lmList individual model fitting')
 
-#import coefficient data from above groupedData as csv
+#import coefficient data from above groupedData as csv, histogram of activation energies
 
-cdata<-read.csv("coef_data.csv")
+cdata<-read.csv("coef_data2.csv")
 
-hist(coef(test)[,2],breaks=30,xlab="Ea")
+hist_acten <-ggplot(cdata, aes(x=Activation_energy, fill=Order))
+act_energies<-hist_acten + geom_bar()
+
+
+#start with simple model (no random effects)
+
+lm0<-lm(ln_rate~X1_kT,data=mdata)
+
+#build mixed effects model (pure cultures only first)
+
+pur_data<-subset(mdata,Culture="Pure")
+
+lm1<-lme(ln_rate~X1_kT,data=pur_data,random=~1|Dataset)
+
+
+
+
