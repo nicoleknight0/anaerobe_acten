214dafed9aeaa889e34c3709b364545da2f645b7
nicoleknight0 <nicoleknight0@gmail.com>
2015-06-19 18:08:34 -0700

    added separate models for mixed and pure datasets

--- mem1.R
+++ mem1.R
@@ -1,4 +1,4 @@
-#mixed effects model to determine activation energies for anaerobic organisms
+(#mixed effects model to determine activation energies for anaerobic organisms
 
 #library(nlme)
 library(ggplot2)
@@ -21,13 +21,19 @@ cdata<-read.csv("coef_data2.csv")
 hist_acten <-ggplot(cdata, aes(x=Activation_energy, fill=Order))
 act_energies<-hist_acten + geom_bar()
 
+pur_cdata<-subset(cdata,Order!="Mixed")
+
+hist_acten_pur <-ggplot(pur_cdata, aes(x=Activation_energy, fill=Order))
+act_energies<-hist_acten_pur + geom_bar()
+
+
 #start with simple model (no random effects)
 
 lm0<-lm(ln_rate~X1_kT,data=mdata)
 
 #build mixed effects model (pure cultures only first)
 
-pur_data<-subset(mdata,Culture="Pure")
+pur_data<-subset(mdata,Culture=="Pure")
 
 lm1<-lme(ln_rate~X1_kT,data=pur_data,random=~1|Dataset)
 
@@ -47,8 +53,29 @@ subdata<-subset(subdata,Rate_type=="Specific growth")
 
 subdata2<-subdata[,c("Dataset","X1_kT","ln_rate")]
 
+
+pur_lm<-lmer(ln_rate~X1_kT+(1+X1_kT|Dataset),data=subdata2)
+#cint_pur<-confint(pur_lm)
+
 acten_subdata<-groupedData(ln_rate~X1_kT|Dataset, subdata2, order.groups=FALSE)
 test_subdata<-lmList(ln_rate~X1_kT|Dataset, data=subdata2, na.action=na.omit)
 pairs(test_subdata, id=0.05)
 
 hist(coef(test_subdata)[,2], breaks=50, xlab='slope', main = 'Slopes from lmList individual model fitting')
+
+
+mix_data<-subset(mdata,Culture=="Mixed")
+
+mix_data2<-mix_data[,c("Dataset","X1_kT","ln_rate")]
+mix_lm<- lmer(ln_rate~X1_kT + (1+X1_kT|Dataset), data = mix_data)
+#cint_mix<-confint(mix_lm)
+
+#see if activation energies of pure and mixed cultures are significantly different
+
+coef_data<-read.csv("coef_data2.csv")
+
+an.1<-aov(Activation_energy~Culture,data=coef_data)
+
+
+hist_acten_cult <-ggplot(coef_data, aes(x=Activation_energy, fill=Type))
+cult_act_energies<-hist_acten_cult + geom_bar()
