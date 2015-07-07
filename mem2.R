9dd486f671e513508650e6b71f02bed9548c025d
Mary <oconnor@zoology.ubc.ca>
2015-07-07 10:39:16 -0700

    added a model pur.lm4 that centers the regressor, and considers only dataset and Order as random effects. Made suggestion about a new predictor column based on Rate_type.

--- mem2.R
+++ mem2.R
@@ -30,10 +30,17 @@ pairs(test, id=0.05)
 #best so far: pur.lm3
 
 pur.lm0<-lmer(ln_rate~X1_kT+(1|Experiment),data=purdata.sub)
-pur.lm0b<-lmer(ln_rate~X1_kT+(X1_kT|Experiment),data=purdata.sub)
+pur.lm0b<-lmer(ln_rate~X1_kT+(X1_kT|Experiment),data=purdata.sub) # pur.lm0 vs pur.lm0b tests whether diff slopes are req.
 pur.lm1<-lmer(ln_rate~X1_kT+(1|Experiment)+(1|ID),data=purdata.sub)
-pur.lm1b<-lmer(ln_rate~X1_kT+(1|Experiment)+(X1_kT|ID),data=purdata.sub)
+pur.lm1b<-lmer(ln_rate~X1_kT+(1|Experiment)+(X1_kT|ID),data=purdata.sub) # pur.lm1 vs pur.lm1b tests whether diff slopes are req.
 pur.lm2<-lmer(ln_rate~X1_kT+(1|Experiment)+(1|ID)+(1|Dataset),data=purdata.sub)
 pur.lm2b<-lmer(ln_rate~X1_kT+(1|Experiment)+(1|ID)+(X1_kT|Dataset),data=purdata.sub)
+
 pur.lm3<-lmer(ln_rate~X1_kT+(1|Experiment)+(1|ID)+(X1_kT|Dataset)+(1|Order),data=purdata.sub)
 pur.lm3b<-lmer(ln_rate~X1_kT+(1|Experiment)+(1|ID)+(X1_kT|Dataset)+(X1_kT|Order),data=purdata.sub,control=lmerControl(optimizer=bobyqa,optctrl = list(maxfun = 100000)))
+
+#This model looks close. correlation between random effects is relatively low (could be lower). 
+# next steps: create a variable that collapses Rate_tupe into as few categories as possible. Maybe metabolite production rates, doublings, and consumption rates? then, a different column for substrates or metabolites (lactate, acetate, nitrate, etc?)
+pur.lm4 <- lmer(ln_rate ~ I(X1_kT-38) + (1|Dataset) + (1 + I(X1_kT-38)|Order), data=purdata.sub)
+
+head(purdata.sub)
