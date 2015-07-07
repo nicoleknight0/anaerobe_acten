577327c53cfe37a44cb5dd2dcf4ec8686d7aee0e
nicoleknight0 <nicoleknight0@gmail.com>
2015-07-07 10:16:59 -0700

    MOAR MODELS

--- mem2.R
+++ mem2.R
@@ -3,22 +3,37 @@ library(lme4)
 
 #look at datasets of pure cultures separately
 
-purdata<-read.csv("analysis_data.csv")
-purdata<-subset(purdata,Culture=="Pure")
+an.data<-read.csv("analysis_data.csv")
+purdata<-subset(an.data,Culture=="Pure")
+mixdata<-subset(an.data,Culture=="Mixed")
 
-purdata.sub<-purdata[,c("ID","Experiment","X1_kT","ln_rate")]
+purdata.sub<-purdata[,c("ID","Experiment","Dataset","X1_kT","ln_rate","pH","Order")]
+mixdata.sub<-mixdata[,c("Experiment","Dataset","X1_kT","ln_rate","pH")]
 
 #calculate the activation energy for all the datasets of pure cultures
 
-pur.acten<-groupedData(ln_rate~X1_kT|ID, purdata.sub, order.groups=FALSE)
-test<-lmList(ln_rate~X1_kT|ID, data=purdata.sub, na.action=na.omit)
+pur.acten<-groupedData(ln_rate~X1_kT|Dataset, purdata.sub, order.groups=FALSE)
+test<-lmList(ln_rate~X1_kT|Dataset, data=purdata.sub, na.action=na.omit)
 pairs(test, id=0.05)
 
-#I'm comparing two linear models.  Both allow slope and intercept to vary with
+#now mixed
+
+mix.acten<-groupedData(ln_rate~X1_kT|Dataset, mixdata.sub, order.groups=FALSE)
+test<-lmList(ln_rate~X1_kT|Dataset, data=mixdata.sub, na.action=na.omit)
+pairs(test, id=0.05)
+
+#Linear models for the pure cultures.
+#All except the first (pur.lm1) allow slope and intercept to vary with
 #species (since we expect metabolic strategy to vary with species, of course)
 #One allow both slope and intercept to vary with Experiment(pur.lm2), the other
 #only allows intercept to vary (pur.lm1).
+#best so far: pur.lm3
 
-pur.lm1<-lmer(ln_rate~X1_kT+(1|Experiment)+(X1_kT|ID),data=purdata.sub)
-pur.lm2<-lmer(ln_rate~X1_kT+(X1_kT|Experiment)+(X1_kT|ID),data=purdata.sub)
-
+pur.lm0<-lmer(ln_rate~X1_kT+(1|Experiment),data=purdata.sub)
+pur.lm0b<-lmer(ln_rate~X1_kT+(X1_kT|Experiment),data=purdata.sub)
+pur.lm1<-lmer(ln_rate~X1_kT+(1|Experiment)+(1|ID),data=purdata.sub)
+pur.lm1b<-lmer(ln_rate~X1_kT+(1|Experiment)+(X1_kT|ID),data=purdata.sub)
+pur.lm2<-lmer(ln_rate~X1_kT+(1|Experiment)+(1|ID)+(1|Dataset),data=purdata.sub)
+pur.lm2b<-lmer(ln_rate~X1_kT+(1|Experiment)+(1|ID)+(X1_kT|Dataset),data=purdata.sub)
+pur.lm3<-lmer(ln_rate~X1_kT+(1|Experiment)+(1|ID)+(X1_kT|Dataset)+(1|Order),data=purdata.sub)
+pur.lm3b<-lmer(ln_rate~X1_kT+(1|Experiment)+(1|ID)+(X1_kT|Dataset)+(X1_kT|Order),data=purdata.sub,control=lmerControl(optimizer=bobyqa,optctrl = list(maxfun = 100000)))
